0bb682026a6eb594c3281b324b8abd8ea5cec08c
William Harvey <wharvey@uw.edu>
2023-06-01 16:11:16 -0700

    csv input

--- scripts/summaries.R
+++ scripts/summaries.R
@@ -10,7 +10,7 @@ suppressPackageStartupMessages(library(plyr))
 source(paste0(snakemake@params[["script_dir"]], "summaries_functions.R"))
 
 #IMPORT DATA
-reads <- read.csv(file=snakemake@input[["reads"]], sep="\t", header = TRUE)
+reads <- read.csv(file=snakemake@input[["csv"]], sep="\t", header = TRUE)
 names(reads) <- c("Call_ID", "Sample_ID", "Chr", "Start", "End", "GT", "Ref_Units", "All1", "All2")
 outpath <- paste0(snakemake@output[["csv"]]
 
