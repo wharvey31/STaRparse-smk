ce79226275e39f035288d25183ad3529b0cb79b8
William Harvey <wharvey@uw.edu>
2023-06-01 16:07:37 -0700

    removed optparse

--- scripts/summaries.R
+++ scripts/summaries.R
@@ -5,20 +5,7 @@ suppressPackageStartupMessages(library(reshape2))
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(plyr))
 
-#       DEFINE ARGUMENTS FOR SCRIPT
-option_list <- list(
-  make_option(c("-d", "--dir"), type="character", action="store", default=NA, help="Path of global directory", metavar="character"),
-  make_option(c("-r", "--reads"), type="character", action="store", default=NA, help="Read file path", metavar="character"),
-  make_option(c("-o", "--out"), type="character", action="store", default=NA, help="output file path and name", metavar="character"),
-  make_option(c("-s", "--savename"), type="character", action="store", help="Indicate job name", metavar="character"),
-  make_option(c("-b", "--build"), type="character", action="store", help="Specify reference genome build (19, 37, or 38)", metavar="character")
-)
 
-opt <- parse_args(OptionParser(option_list=option_list))
-
-#       ARGUMENT CHECK
-
-  
     #IMPORT FUNCTIONS
 source(paste0(snakemake@params[["script_dir"]], "summaries_functions.R"))
 
